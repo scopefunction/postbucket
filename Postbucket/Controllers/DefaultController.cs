88e5ee075c1af2021aaf3db4f3ec133036f413d4
Caybo Kotze <kotzecabo@gmail.com>
2021-04-19 23:07:44 +0200

    :sparkles: Add default controller and remove htsRedirect

---
+++ Postbucket/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Postbucket.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class DefaultController : ControllerBase
+    {
+        [Route("")]
+        public void Default()
+        {
+            HttpContext.Response.ContentType = "text/html";
+            HttpContext.Response.WriteAsync("<html><h1>Postbucket</h1></html>");
+        }
+    }
+}
