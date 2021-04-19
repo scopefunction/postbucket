fb2e71b992edd1ed249146ec92de8b90801bef67
Jessica Jane <jessica@thebitfoundry.net>
2021-04-19 15:10:01 +0200

    Update EmailService.cs

--- Postbucket/BLL/EmailService.cs
+++ Postbucket/BLL/EmailService.cs
@@ -8,7 +8,7 @@ namespace Postbucket.BLL
 {
     public class EmailService
     {
-        public static void Send(string body, string sender)
+        public static void Send(string body, string recipient)
         {
             var client = new SmtpClient("smtp.mailtrap.io", 2525)
             {
@@ -16,10 +16,10 @@ namespace Postbucket.BLL
                 EnableSsl = true
             };
             
-            client.Send("mail@mergedigital.io", sender, "New Form Submission", body);
+            client.Send("mail@mergedigital.io", recipient, "New Form Submission", body);
         }
      
-        public static IRestResponse SendSimpleEmail(string body, string sender)
+        public static IRestResponse SendSimpleEmail(string body, string recipient)
         {
             RestClient client = new RestClient ();
             client.BaseUrl = new Uri ("https://api.mailgun.net/v3/sandbox9c2e9e7b1d7843da86746125faf83950.mailgun.org");
@@ -30,7 +30,7 @@ namespace Postbucket.BLL
             request.AddParameter ("domain", "mail.merge.africa", ParameterType.UrlSegment);
             request.Resource = "{domain}/messages";
             request.AddParameter ("from", "Excited User <mailgun@mail@merge.africa>");
-            request.AddParameter ("to", sender);
+            request.AddParameter ("to", recipient);
             request.AddParameter ("subject", "Hello");
             request.AddParameter ("text", body);
             request.Method = Method.POST;
