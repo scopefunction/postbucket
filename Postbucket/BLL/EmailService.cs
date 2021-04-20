fd97fc4907d7f78c2ee4beefdcea6a37f682c17f
Caybo Kotze <kotzecabo@gmail.com>
2021-04-20 20:45:25 +0200

    :sparkles: Change api key to env var

--- Postbucket/BLL/EmailService.cs
+++ Postbucket/BLL/EmailService.cs
@@ -23,9 +23,9 @@ namespace Postbucket.BLL
         {
             RestClient client = new RestClient ();
             client.BaseUrl = new Uri ("https://api.mailgun.net/v3/sandbox9c2e9e7b1d7843da86746125faf83950.mailgun.org");
+            
             client.Authenticator =
-            new HttpBasicAuthenticator ("api",
-                "1ab3bc0a5acb8a2fc03680c600ba789a-1b6eb03d-13f17028");
+            new HttpBasicAuthenticator("api", Environment.GetEnvironmentVariable("MAILGUN_API_KEY") ?? string.Empty);
             RestRequest request = new RestRequest ();
             request.AddParameter ("domain", "mail.merge.africa", ParameterType.UrlSegment);
             request.Resource = "{domain}/messages";
